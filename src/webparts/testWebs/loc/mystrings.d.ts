ceeda863d099145836f0f84edacb6302e835a33d
Jason Clark <jason_mc@btinternet.com>
2023-09-30 16:23:59 +0100

    first commit

---
+++ src/webparts/testWebs/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITestWebsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TestWebsWebPartStrings' {
+  const strings: ITestWebsWebPartStrings;
+  export = strings;
+}
